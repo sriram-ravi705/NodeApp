d0de783797112ab4043c535c22a13cd951cb5014
sriram-ravi705 <sriramravi477@gmail.com>
2025-01-29 21:55:43 +0530

    jenkinsfile addes

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ echo 'starting deployment.......'
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 266735803751.dkr.ecr.us-east-1.amazonaws.com
 
-docker build 266735803751.dkr.ecr.us-east-1.amazonaws.com/nodeapp:latest .
+docker build -t 266735803751.dkr.ecr.us-east-1.amazonaws.com/nodeapp:latest .
 
 docker push 266735803751.dkr.ecr.us-east-1.amazonaws.com/nodeapp:latest
 
